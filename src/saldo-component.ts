c97aa9796be8c4e0aa99adf4e03a876c6a74fb43
Evelyn Lima <vinhahh.3000@gmail.com>
2025-06-13 11:27:16 -0300

    Formatação da moeda.

--- src/saldo-component.ts
+++ src/saldo-component.ts
@@ -1,6 +1,6 @@
-let saldo = 3000;
+let saldo: number = 3000;
 
 const elementoSaldo = document.querySelector(".saldo-valor .valor") as HTMLElement;
 if (elementoSaldo !== null) {
-elementoSaldo.textContent = saldo.toString();
+elementoSaldo.textContent = saldo.toLocaleString("pt-br", {currency: "BRL", style: "currency"});
 }
