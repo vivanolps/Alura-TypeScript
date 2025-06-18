920155b86415865bb63d6518ac54cbc4fe433d10
Evelyn Lima <vinhahh.3000@gmail.com>
2025-06-18 10:51:39 -0300

    Exportando recursos para uso externo.

--- src/components/saldo-component.ts
+++ src/components/saldo-component.ts
@@ -11,3 +11,4 @@ if (elementoDataAcesso !== null) {
     const dataAcesso: Date = new Date();
     elementoDataAcesso.textContent = formatarData(dataAcesso, FormatoData.DIA_SEMANA_DIA_MES_ANO);
 }
+
